15253949bba545ab3b1d8aabe2754265228ae89d
wy200088 <442774512@qq.com>
2019-10-14 13:46:42 +0800

    Site updated: 2019-10-14 13:46:41

--- dist/music.js
+++ dist/music.js
@@ -3,11 +3,6 @@ const ap = new APlayer({
     fixed: true,
     autoplay: true,
     audio: [
-      {
-        name: "Hey Kong",
-        artist: 'Key.L����',
-        url: 'http://m10.music.126.net/20191014131916/b9c6747d5c769729226f371ce6dcc0d6/yyaac/015e/555d/0153/644df2e32ab78b8eebfc69fdb3b1c658.m4a',
-        cover: 'http://p2.music.126.net/6_bZAvi7pi7V9U0JnvS8uQ==/109951164298338067.jpg?param=130y130',
-      },
+     {% aplayer ��Hey Kong�� ��Key.L�� ��http://m10.music.126.net/20191014135742/72dda483d7b708d21a96313828136173/yyaac/015e/555d/0153/644df2e32ab78b8eebfc69fdb3b1c658.m4a�� ��http://p2.music.126.net/6_bZAvi7pi7V9U0JnvS8uQ==/109951164298338067.jpg?param=130y130�� ��autoplay�� %}
     ]
 });
